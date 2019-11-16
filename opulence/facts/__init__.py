76ea346f31ca20a74daccd7817003f19918a6834
louis jurczyk <louis.jurczyk@epitech.eu>
2019-11-16 23:45:07 +0100

    Make facts importable

---
+++ opulence/facts/__init__.py
@@ -0,0 +1,9 @@
+from .domain import Domain
+from .ip import IPv4, IPv6
+from .person import Person
+from .port import Port
+from .socialProfile import SocialProfile
+from .username import Username
+
+
+__all__ = [Username, SocialProfile, Port, Person, IPv6, IPv4, Domain]
