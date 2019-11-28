b8a47a2ab0df187daea393efbb2a079aeab95fb9
louis jurczyk <louis.jurczyk@epitech.eu>
2019-11-29 00:23:03 +0100

    facts: fix line too long in __init__

--- opulence/facts/__init__.py
+++ opulence/facts/__init__.py
@@ -1,10 +1,20 @@
 from .domain import Domain
+from .file import File
 from .ip import IPv4, IPv6
+from .operatingSystem import OperatingSystem
 from .person import Person
 from .port import Port
 from .socialProfile import SocialProfile
 from .username import Username
-from .operatingSystem import OperatingSystem
-from .file import File
 
-__all__ = [Username, SocialProfile, Port, Person, IPv6, IPv4, Domain, OperatingSystem, File]
+__all__ = [
+    Username,
+    SocialProfile,
+    Port,
+    Person,
+    IPv6,
+    IPv4,
+    Domain,
+    OperatingSystem,
+    File,
+]
